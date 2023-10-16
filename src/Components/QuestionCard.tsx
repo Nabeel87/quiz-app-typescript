bb701ce686a8af3cdc2d5a608926404204e0cc20
Nabell87 <nabilsabir786@gmail.com>
2023-10-16 21:45:55 +0500

    Create QuestionCard.tsx

---
+++ src/Components/QuestionCard.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { QuizType } from "../Types/quiz_type";
+
+const QuestionCard: React.FC<any> = ({
+    question,
+    options
+}) => {
+    console.log(options);
+    return (
+        <div className="question-container">
+            <h1>hello</h1>
+        </div>
+    )
+}
+
+export default QuestionCard;
